fe29a59d40de96ed4f06d70d07aa0021708216f5
Joe Whitwell <joewhitwell@hotmail.com>
2017-06-01 00:27:30 +0000

    Updated directory

--- directory.rb
+++ directory.rb
@@ -1,79 +1,32 @@
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
+def interactive_menu
   students = []
-  # get the first name
-  name = gets.chomp
-  
-  
-  
-  
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-  # return the array of students
-  students
-end
-
-#def input_students
-#  puts "Please enter the hobby of the students"
-#  puts "To finish, just hit return twice"
-  # create an empty array
-#  hobby = []
-  # get the first name
-#  name = gets.chomp
-  
-  
-  
-  
-  # while the name is not empty, repeat this code
-#  while !name.empty? do
-    # add the student hash to the array
-#    students << {name: name, cohort: :november}
-#    puts "Now we have #{students.count} students"
-#    # get another name from the user
-#    name = gets.chomp
-#  end
-  # return the array of students
-#  students
-#end
-
-
-
-
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-
-
-def print(students)
-  students.each_with_index do |student|
-    puts "#{student[:name]} #{student[:hobbies]} #{student[:"country of birth"]} #{student[:height]} (#{student[:cohort]} cohort)"
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    
+    when "1"
+      students = input_students
+      
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+      
+    when "9"
+        
+      exit # terminate program
+      
+    else
+      puts "I don't know what you meant, try again"
+    end
   end
 end
 
-#while 
 
 
-
-#.starts_with("f")
-# truncate
-
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end
-
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
